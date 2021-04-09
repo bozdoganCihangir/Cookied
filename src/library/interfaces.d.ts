025cb4acb3345a29368a30bacd7663a20a441c10
Cihangir Bozdogan <cihangirrbozzd@gmail.com>
2021-04-09 18:44:36 +0300

    refactor: window global type added

--- src/library/interfaces.d.ts
+++ src/library/interfaces.d.ts
@@ -1,10 +1,14 @@
+declare global {
+    interface Window {
+        cookied: any;
+    }
+}
+
 export interface CookiesList {
     [x: string]: string;
 }
 
 export interface CookieOptions {
-    path?: string;
-    domain?: string;
     expires?: any;
     secure?: boolean;
 }

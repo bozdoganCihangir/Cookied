f6b1fcd6b9d46f14739954c3e57d684b235ad5e8
Cihangir Bozdogan <cihangirrbozzd@gmail.com>
2021-04-09 18:42:13 +0300

    refactor: constants moved inside utils.ts

--- src/library/utils.ts
+++ src/library/utils.ts
@@ -1,5 +1,13 @@
-import { BROWSER_PER_COOKIE_BYTE_LIMIT } from "./constants";
 import { CookieOptions } from "./interfaces";
+
+/**
+ * Web Browsers Maximum size per cookie
+ * Google Chrome 18040 bytes
+ * Firefox	15040 bytes
+ * Opera 18040 bytes
+ * Android	5040 bytes
+ */
+const BROWSER_PER_COOKIE_BYTE_LIMIT = 5040;
 
 export const convertToByte = (val: string) => {
     const matches = encodeURIComponent(val).match(/%[89ABab]/g);
@@ -17,12 +25,10 @@ export const calcExpire = (expire: string): string => {
     const indicator = expire.split("")[expire.length - 1];
     const duration = parseInt(expire.substring(0, expire.length - 1), 10);
 
-    // hour
     if (indicator === "h") {
         time = 60 * 60 * 1000 * duration;
     }
 
-    // day
     if (indicator === "d") {
         time = 24 * 60 * 60 * 1000 * duration;
     }
@@ -34,14 +40,6 @@ export const calcExpire = (expire: string): string => {
 export const buildCookie = (cookie: string, options: CookieOptions): string => {
     if (options.expires) {
         cookie += ";" + "expires=" + calcExpire(options.expires);
-    }
-
-    if (options.domain) {
-        cookie += ";" + "domain=" + options.domain;
-    }
-
-    if (options.path) {
-        cookie += ";" + "path=" + options.path;
     }
 
     if (options.secure) {
